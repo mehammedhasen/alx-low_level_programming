deb7392ef0c3f3755a4ed644badc8d8f01d6d824
mehammedhasen <mehammedhasen@yahoo.com>
2023-10-30 11:52:22 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0
+*/
+int main(void)
+{char c;
+int n;
+c = 'a';
+n = 0;
+while (n < 10)
+{putchar (n + '0');
+n++;
+}
+while (c <= 'f')
+{putchar (c);
+c++;
+}
+putchar ('\n');
+return (0);
+}
