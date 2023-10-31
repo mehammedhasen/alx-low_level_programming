b9c1d0b15f3ad1a079602988bd46fc2db705771e
mehammedhasen <mehammedhasen@yahoo.com>
2023-10-30 20:16:41 -0700

    101printComb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,15 +7,23 @@
 int main(void)
 {int ones = '0';
 int tens = '0';
+int hundr = '0';
+for (hundr = '0'; hundr <= '9'; hundr++)
+{
 for (tens = '0'; tens <= '9'; tens++)
 {
 for (ones = '0'; ones <= '9'; ones++)
-if (!((ones == tens) || (tens > ones)))
-{putchar (tens);
+{
+if (!((ones == tens) || (tens == hundr) || (hundr > tens)))
+{
+putchar (hundr);
+putchar (tens);
 putchar (ones);
-if (!(ones == '9' && tens == '8'))
+if (!(ones == '9' && hundr == '7' && tens == '8'))
 {putchar (',');
 putchar (' ');
+}
+}
 }
 }
 }
