ee7604fc7ae5b0da41dea6f31405cf3833732785
mehammedhasen <mehammedhasen@yahoo.com>
2023-11-01 19:32:03 -0700

    Return absolute value code

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,15 @@
-i
+#include "main.h"
+/**
+ * _abs - prints the sign of a number
+ * @n: the number will be checked
+ * Return: Absolute value of number
+ */
+int _abs(int n)
+{
+if (n < 0)
+{int abs_val;
+abs_val = n * -1;
+return (abs_val);
+}
+return (n);
+}
