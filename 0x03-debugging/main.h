1d635e520e6d44aa2906ad030ecd385758547da7
mehammedhasen <mehammedhasen@yahoo.com>
2023-11-03 06:47:06 -0700

    Update on positive negative

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,6 +2,22 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include "main.h"
 void positive_or_negative(int i);
+/**
+* positive_or_negative - test that prints if integer is positive or negative
+* @i: inte
+* Return: void
+*/
+
+void positive_or_negative(int i)
+{
+if (i > 0)
+printf("%d is positive\n", i);
+else if (i == 0)
+printf("%d is zero\n", i);
+else
+printf("%d is negative\n", i);
+}
 
 #endif /* MAIN_H */
