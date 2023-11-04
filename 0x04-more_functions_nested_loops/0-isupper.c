2e67123fb07e3da0ea4e10584ddbf69e62674f33
mehammedhasen <mehammedhasen@yahoo.com>
2023-11-03 20:02:46 -0700

    case check upper or lower

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isupper - check that if upper or lower case
+* @i: number to be check
+* Return: 1 for upper else 0
+*/
+
+int _isupper(int i)
+{
+if (i >= 65 && i <= 90)
+{return (1);
+}
+return (0);
+}
