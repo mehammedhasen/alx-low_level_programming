092a1ca1ac86792cb0ea83343d7f58414b185fc1
mehammedhasen <mehammedhasen@yahoo.com>
2023-11-06 09:48:23 -0800

    3puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _puts - function that returns a string
+* @str: input string
+* Return: void
+*/
+void _puts(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+_putchar (str[i]);
+_putchar ('\n');
+}
