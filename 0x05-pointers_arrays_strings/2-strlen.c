740c321c9b89126bfc616747c4247d51489ad4ab
mehammedhasen <mehammedhasen@yahoo.com>
2023-11-06 09:35:51 -0800

    2-strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,12 @@
 /**
 * _strlen - function that returns length of a string
 * @s: input string
-* @c: starting from 0
 * Return: leng
 */
 int  _strlen(char *s)
 {
-int leng = 0;
 int i;
+int leng = 0;
 for (i = 0; s[i] != '\0'; i++)
 leng++;
 return (leng);
