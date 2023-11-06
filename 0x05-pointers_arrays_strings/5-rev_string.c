1a43f982d00705b55c992fa37d1365a3e6a98d24
mehammedhasen <mehammedhasen@yahoo.com>
2023-11-06 11:01:33 -0800

    5-rev-string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* rev_string - Entry point
+* @s: input string
+* Return: void
+*/
+void rev_string(char *s)
+{
+int i;
+int leng = 0;
+for (i = 0; s[i] != '\0'; i++)
+leng++;
+for (i = 0; i < leng / 2; i++)
+{
+char j;
+j = s[i];
+s[i] = s[leng - 1 - i];
+s[count - 1 - i] = j;
+}
+}
