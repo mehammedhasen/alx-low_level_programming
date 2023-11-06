c7eb472c51c10a271470a1f636a61ef6564010f0
mehammedhasen <mehammedhasen@yahoo.com>
2023-11-06 12:01:28 -0800

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+i#include "main.h"
+/**
+* puts_half - function prints half string
+* @str: first input
+* Return: void
+*/
+
+void puts_half(char *str)
+{
+int i;
+int n;
+int leng = 0;
+
+for (i = 0; str[i] != '\0'; i++)
+{leng++;
+}
+n = (leng - 1) / 2;
+for (i = n + 1; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
