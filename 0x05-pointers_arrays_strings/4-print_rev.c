370942a076c8159d3ed093ea826de981181c3beb
mehammedhasen <mehammedhasen@yahoo.com>
2023-11-06 10:09:36 -0800

    4printReverse q

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* print_rev - Entry point
+* @s: input string
+* Return: void
+*/
+void print_rev(char *s)
+{
+int i;
+int leng = 0;
+for (i = 0; s[i] != '\n'; i++)
+leng++;
+for (i = leng - 1; i >= 0; i--)
+{
+_putchar (s[i]);
+}
+_putchar ('\n');
+}
