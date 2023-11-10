542d9b7ce7a0b95d338860e2b82eacb9db7defeb
mehammedhasen <mehammedhasen@yahoo.com>
2023-11-10 11:21:17 -0800

    2strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strncpy - Entry point
+* @dest: first input
+* @src: second input
+* @n: 3rd input
+* Return: dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int len = strlen(dest);
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+while (i < n)
+{dest[i] = '\0';
+i++;
+}
+}
+return (dest);
+}
