39e3334f649568fadd75bb59431c019324f00ea6
mehammedhasen <mehammedhasen@yahoo.com>
2023-11-10 11:00:33 -0800

    1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strncat - Entry point
+* @dest: first input
+* @*src: second input
+* @n: 3rd input
+* Return: dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+int len = strlen(dest);
+int i;
+
+for (i = 0; i < n && *src != '\0'; i++)
+{
+dest[len + i] = *src;
+src++;
+}
+dest[len + i] = '\0';
+return (dest);
+}
