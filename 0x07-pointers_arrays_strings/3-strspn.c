2b67ffb03585d86c98204b31998c142c4df09474
mehammedhasen <mehammedhasen@yahoo.com>
2023-11-13 12:01:14 -0800

    3-strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strspn - length of prefix
+* @s: first input
+* @accept: second input
+* Return: Always 0
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, n, check, value = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+check = 0;
+for (n = 0; accept[n] != '\0'; n++)
+{
+if (accept[n] == s[i])
+value++;
+check = 1;
+}
+}
+}
