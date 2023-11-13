11a47fae9a00116e18f9bd2f5c0c15ec74be355b
mehammedhasen <mehammedhasen@yahoo.com>
2023-11-13 11:11:57 -0800

    1-memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _memcpy - copy memory area
+* @dest: memory sored
+* @src: memory copied
+* @n: bytes
+* Return: copied memory n byte changed
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int j = 0;
+int i = n;
+for (; j < i; j++)
+{
+dest[j] = src[j];
+j--;
+}
+return (dest);
+}
