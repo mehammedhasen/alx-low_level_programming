43b9600f70b499d007300c0b6531130a79c3ae77
mehammedhasen <mehammedhasen@yahoo.com>
2023-11-13 11:39:35 -0800

    1-memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,13 +5,15 @@
 * @dest: memory sored
 * @src: memory copied
 * @n: bytes
-* Return: copied memory n byte changed
+* Return: Always 0 copied memory n byte changed
 */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
-for (; i < n; i++)
+for (i = 0; i < n; i++)
+{
 dest[i] = src[i];
+}
 return (dest);
 }
