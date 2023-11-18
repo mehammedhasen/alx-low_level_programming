9214e09ada8cdd7a3b93422deb2bbeea6ecb4181
mehammedhasen <mehammedhasen@yahoo.com>
2023-11-17 18:47:46 -0800

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - Entry point of function
+* @argc: first ag
+* @argv: second arg
+* Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+printf("%s\n", argv[0]);
+return (0);
+}
