7d40211dfadb2d1b82bf80528b42ef8c1168fa9f
mehammedhasen <mehammedhasen@yahoo.com>
2023-11-19 09:54:57 -0800

    3-mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry Point
+ * _atoi is a function that converts a string into an int
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int i, result = 1;
+if (argc != 3)
+{
+printf("%s\n", "Error");
+return (1);
+}
+else
+{
+for (i = 1; i < argc; i++)
+{
+result *= _atoi(argv[i]);
+}
+printf("%d\n", result);
+}
+return (0);
+}
