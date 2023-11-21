775bd6e85c50cf2d55eabe5f661b8cc810dcf7a4
mehammedhasen <mehammedhasen@yahoo.com>
2023-11-20 18:51:13 -0800

    1-RGS

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,13 +8,11 @@
  */
 int main(int argc, char *argv[])
 {
-int i;
-if (argc == 1)
-printf("%d\n", argc - 1);
-else
+int count = 0;
+while (argv[count] != NULL)
 {
-for (i = 0; *argv; i++, argv++)
-printf("%d\n", i - 1);
+count++;
 }
+printf("Number of arguments: %d\n", count - 1);
 return (0);
 }
