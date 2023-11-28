4c28074a3e50e4ebad1fbdb9a871f2eb9d653500
mehammedhasen <mehammedhasen@yahoo.com>
2023-11-28 15:44:50 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a Dog definition
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: Dog Definition
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
