f0bdc32b72e6843500418c5197b61a0ad26c7367
mehammedhasen <mehammedhasen@yahoo.com>
2023-11-28 16:56:42 -0800

    4-new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,15 +6,43 @@
  * @name: dog name
  * @age: dog's age.
  * @owner: dog's owner
+ * Return: pointer to new dog on success
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-struct dog *d;
-d = malloc(sizeof(struct dog));
-if (d == NULL)
+int i, lenN, lenO;
+struct dog *n_dog = NULL;
+lenN = 0;
+while (name[lenN] != '\0')
+lenN++;
+lenO = 0;
+while (owner[lenO] != '\0')
+lenO++;
+n_dog = malloc(sizeof(struct dog));
+if (n_dog == NULL)
+{
+free(n_dog);
 return (NULL);
-d->name = name;
-d->age = age;
-d->owner = owner;
-return (d);
+}
+n_dog->name = malloc(lenN + 1);
+if (n_dog->name == NULL)
+{
+free(n_dog->name);
+free(n_dog);
+return (NULL);
+}
+n_dog->owner = malloc(lenO + 1);
+if (n_dog->owner == NULL)
+{
+free(n_dog->name);
+free(n_dog->owner);
+free(n_dog);
+return (NULL);
+}
+for (i = 0; i <= lenN; i++)
+n_dog->name[i] = name[i];
+for (i = 0; i <= lenO; i++)
+n_dog->owner[i] = owner[i];
+n_dog->age = age;
+return (n_dog);
 }
